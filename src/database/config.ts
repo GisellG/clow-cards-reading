0a798c9630af1fd4d15f6038a239c9f21e8d9379
GisellG <gisellp.gutierrezl@gmail.com>
2022-11-24 22:37:58 -0500

    Revert service config

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,8 +4,14 @@ require('dotenv').config();
 export const dbConnection = async () => {
     const dbKey = process.env.MONGODB_CNN as string;
 
-    mongoose.connect(dbKey).
-        catch(e => { throw new Error(e) });
+    try {
+        const db = await mongoose.connect(dbKey);
+        console.log('Database online ⭐! The DB is connected to', db.connection.name);
+
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error at initializing the database');
+    }
 };
 
 
